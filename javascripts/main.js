7992382445a44df03d61155cf2d5937c40228f6f
James <CrazyPython@users.noreply.github.com>
2016-03-09 10:03:23 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,7 @@
 console.log('This would be the main JS file.');
+ $(function(){
+      $(".element").typed({
+        strings: ["First sentence.", "Second sentence."],
+        typeSpeed: 0
+      });
+  });
