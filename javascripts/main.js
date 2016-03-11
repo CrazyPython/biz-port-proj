4867b53a2d27c3faa7459c1e4e20981f97a3b80a
James <CrazyPython@users.noreply.github.com>
2016-03-11 09:55:39 -0500

    Create gh-pages branch via GitHub

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,1 @@
 console.log('This would be the main JS file.');
- $(function(){
-      $(".element").typed({
-        strings: ["First sentence.", "Second sentence."],
-        typeSpeed: 0
-      });
-  });
